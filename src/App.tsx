14497aeaeb8c17260da009a82f2d3ca72f98f34c
frank245chikanku <franklinchikanku58@gmail.com>
2024-05-08 19:07:36 +0200

    navbar done

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,16 @@
 import './App.css'
 import Register from './components/Register'
 import Signup from './components/Signup'
-import {Routes,Route} from "react-router-dom"
+import { Routes, Route } from "react-router-dom"
+import Home from './pages/Home'
 
 function App() {
 
   return (
     <Routes>
-   <Route path="/" element={ <Signup/>}/>
-   <Route path="/register" element={ <Register/>}/>
+      <Route path="/" element={<Home />} />
+      <Route path="/login" element={<Signup />} />
+      <Route path="/register" element={<Register />} />
     </Routes>
   )
 }
---
+++ src/pages/Home.tsx
@@ -0,0 +1,11 @@
+import Navbar from "../components/global/Navbar"
+
+const Home = () => {
+  return (
+    <div>
+        <Navbar/>
+    </div>
+  )
+}
+
+export default Home
